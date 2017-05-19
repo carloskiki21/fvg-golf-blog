7757c5ceef5bb8e471d8b98e09d0ee86e3ad0cbc
Carlos Gonzalez <carlosegonzax@gmail.com>
2017-05-18 20:58:34 -0400

    18/05

--- core/bin/ajax/goLogin.php
+++ core/bin/ajax/goLogin.php
@@ -1,6 +1,23 @@
 <?php
 
-echo $_POST['user'].'<br>';
-echo $_POST['pass'];
+if(!empty($_POST['user']) and !empty($_POST['pass'])) {
+  $db = new Conexion();
+  $data = $db->real_escape_string($_POST['user_login']);
+  $pass = $_POST['pass'];
+  $sql = $db->query("SELECT id FROM users WHERE (user='$data' OR email='$data') AND pass='$pass' LIMIT 1;");
+  if($db->rows($sql) > 0) {
+    echo 1;
+  } else {
+    echo '<div>
+    <strong>ERROR:</strong> Las credenciales son incorrectas.
+  </div>';
+  }
+  $db->liberar($sql);
+  $db->close();
+} else {
+  echo '<div">
+  <strong>ERROR:</strong> Todos los datos deben estar llenos.
+</div>';
+}
 
 ?>
--- core/core.php
+++ core/core.php
@@ -3,7 +3,13 @@ session_start();
 
 #App constants
 define('APP_TITTLE','FVG - Golf Venezolano');
-define('APP_URL','http://localhost/fvg-golf-blog/');
+define('APP_URL','http://localhost/Apps/fvg-golf-blog/');
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','ocrendbb');
+require('core/models/class.Conexion.php');
 
 #Functions required for all the app
 require('core/bin/functions/Users.php');
